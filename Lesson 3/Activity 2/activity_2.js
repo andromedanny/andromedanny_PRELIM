1d233d4e0cf8a5b77ba2af612ab4647fd6e74fbd
John Daniel Marquez <prodanny80@gmail.com>
2024-09-20 20:05:07 +0800

    MARQUEZ, JOHN DANIEL M. - ACTIVITY 3

---
+++ Lesson 3/Activity 2/activity_2.js
@@ -0,0 +1,42 @@
+//part 1
+//task 1
+let name = "Danny";
+let age = 22;
+let isStudent = true;
+//task 2
+console.log(name); 
+console.log(age); 
+console.log(isStudent); 
+//part 2
+//task 1
+let person = {
+    firstName: "Danny",
+    lastName: "Qram",
+    age: 22
+    };
+
+    console.log(person);
+//task 2
+    let colors = ["red", "green", "white"];
+    console.log(colors[1]); 
+//part 3
+//task 1
+    let num1 = 8;
+    let num2 = 4;
+console.log("Addition:"+ (num1 + num2));
+console.log("Subtraction:"+ (num1 - num2));
+console.log("Multiplication:" + (num1 * num2));
+console.log("Division:"+ (num1 / num2));
+//part 4
+//task 1
+let firstName = "John ";
+let lastName = "Qram";
+let fullName = firstName + " "+ lastName;
+console.log(fullName); 
+//part 5
+//task 1
+console.log(typeof name); 
+console.log(typeof age); 
+console.log(typeof isStudent); 
+console.log(typeof person); 
+console.log(typeof colors); 
---
+++ Lesson 3/Machine Problem 2/machine_problem_2.js
@@ -0,0 +1,43 @@
+//part 1
+let userName = "JohnMarq";
+let userAge = 25;
+let isLoggedIn = true;
+let userScores = [85, 92, 78, 90];
+let userProfile = {
+    name: "JohnMarq",
+    age: 25,
+    isActive: true
+};
+
+//task 2
+console.log(userName);
+console.log(userAge);
+console.log(isLoggedIn);
+console.log(userScores);
+console.log(userProfile);
+//part 2
+//task 1
+let num1 = 10;
+let num2 = 5;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+//task 2
+let firstName = "John";
+let lastName = "Qram";
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+//part 3
+//task 1
+console.log(typeof userName);
+console.log(typeof userAge);
+console.log(typeof isLoggedIn);
+console.log(typeof userScores);
+console.log(typeof userProfile);
+//part 4
+//task 1
+console.log(userProfile.name);
+console.log(userProfile.age);
+console.log(userProfile.isActive);
